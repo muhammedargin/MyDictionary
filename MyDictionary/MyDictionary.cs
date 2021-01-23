ad3823680f259c7d984c088ad65091ef0bf5e322
Muhammed Argın <yorgunargin@outlook.com>
2021-01-23 21:01:49 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<TKey,TValue>
+    {
+        TKey[] keys;
+        TValue[] values;
+
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] tempKeys = keys;
+            TValue[] tempValues = values;
+            keys = new TKey[keys.Length + 1];
+            values = new TValue[values.Length + 1];
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+            }
+
+            keys[keys.Length - 1] = key;
+
+            for (int i = 0; i < tempValues.Length; i++)
+            {
+                values[i] = tempValues[i];
+            }
+
+            values[values.Length - 1] = value;
+
+
+        }
+
+        private int myVar;
+
+        public TKey[] Keys
+        {
+            get { return keys; }
+           
+        }
+
+        public TValue[] Values
+        {
+            get { return values; }
+
+        }
+
+
+
+    }
+}
+
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> carsOfEmployees = new MyDictionary<int,string>();
+
+            carsOfEmployees.Add(1, "Honda Civic: 16TT898");
+            carsOfEmployees.Add(2, "Renault Clio 2 : 41AD219");
+
+            foreach (var item in carsOfEmployees.Keys)
+            {
+
+                Console.WriteLine(item);
+            }
+
+            foreach (var item in carsOfEmployees.Values)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
